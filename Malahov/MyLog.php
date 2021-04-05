391b6208101843df4fd15430a7162965efec0443
oleg <aa>
2021-04-05 15:31:18 +0300

    lab9

--- Malahov/MyLog.php
+++ Malahov/MyLog.php
@@ -6,6 +6,8 @@ use core\LogInterface;
 
 Class MyLog extends LogAbstract implements LogInterface {
 
+
+
     public function _log(String $str){
         $this->log[]=$str;
     }
@@ -13,19 +15,24 @@ Class MyLog extends LogAbstract implements LogInterface {
     /**
      * @param String $str строка для записи в массив лога
      */
-    public static function log(String $str){
+    public static function log(string $str):void{
         self::Instance()->_log($str);
 	}
     
     public function _write(){
-        
+
+
+        $d = date_create();
+        $z = (string)$d->format('d-m-Y\TH.i.s.u');
         foreach($this->log as $value){
             echo $value."\n";
+
+            file_put_contents(__DIR__ . "\..\log\\".$z.".log", trim($value."\r\n") . PHP_EOL, FILE_APPEND);
         }
         
     }
     
-    public static function write(){
+    public static function write():void{
         self::Instance()->_write();
     }
 
---
+++ build.php
@@ -0,0 +1,4 @@
+<?php
+
+shell_exec('git clone https://github.com/rok9ru/trpo-core.git core');
+shell_exec('git symbolic-ref --short -q HEAD>version');
