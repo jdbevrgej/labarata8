1fbd56a2cb72955f1f4f820b79774370e5580e61
oleg <aa>
2021-04-05 15:26:17 +0300

    4

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
 <?php
-namespace Laba3;
 
 use Malahov\MyLog;
+use Malahov\LoginovException;
+use Malahov\QuEquation;
 
-require_once "core\EquationInterface.php";
-require_once "core\LogAbstract.php";
-require_once "core\LogInterface.php";
-require_once "Loginov\Line.php";
-require_once "Loginov\MyLog.php";
-require_once "Loginov\Quadratic.php";
-MyLog::log("vadim");
+include "core/EquationInterface.php";
+include "core/LogAbstract.php";
+include "core/LogInterface.php";
+include "Malahov/MyLog.php";
+include "Malahov/Equation.php";
+include "Malahov/QuEquation.php";
+include "Malahov/Exception.php";
+
+ini_set("display_errors", 1);
+error_reporting(-1);
+
+try {
+    $b = new QuEquation();
+    $values = array();
+
+    for ($i = 1; $i < 4; $i++) {
+        echo "Введите " . $i . " аргумент: ";
+        $values[] = readline();
+    }
+    $va = $values[0];
+    $vb = $values[1];
+    $vc = $values[2];
+
+    MyLog::log("Введено уравнение " . $va . "x^2 + " . $vb . "x + " . $vc . " = 0");
+    $x = $b->solve($va, $vb, $vc);
+
+    $str = implode(", ", $x);
+    MyLog::log("Корни уравнения: " . $str);
+} catch (Exception $e) {
+    MyLog::log($e->getMessage());
+}
+
 MyLog::write();
+
+?>
